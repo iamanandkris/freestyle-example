afbdb8afdce60375ce23f1cf13ffadbca60476c1
Abdhesh Kumar <abdhesh@callhandling.co.uk>
2017-04-10 17:51:27 +0100

    Write Monad for Task

---
+++ src/main/scala/com/abtechsoft/TaskInterpreters.scala
@@ -0,0 +1,52 @@
+package com.abtechsoft
+
+import cats.data.Kleisli
+import monix.eval.Task
+
+import com.abtechsoft.Algebras._
+
+/**
+  * Created by abdhesh on 06/04/17.
+  */
+object TaskInterpreters {
+  type ParMixedTask[A] = Kleisli[Task, Unit, A]
+
+  implicit val arithInterp = new Arith.Handler[ParMixedTask]{
+    def add(x:Int,y:Int):ParMixedTask[Int] = Kleisli(s =>Task.pure(x+y))
+    def subtract(x:Int,y:Int):ParMixedTask[Int] = Kleisli(s =>Task.pure(x - y))
+  }
+
+  implicit val sagaInterp = new SagaOP.Handler[ParMixedTask]{
+    def updateAccount(account:(String,String,Double)):ParMixedTask[Boolean] = Kleisli(s =>Task.pure(true))
+    def updateUser(user:(String,String,Int)):ParMixedTask[Boolean] = Kleisli(s =>Task.pure(true))
+  }
+
+
+
+  implicit val dbOpInterp = new DBOperation.Handler[ParMixedTask]{
+    def getUserById(key:String):ParMixedTask[(String,String,Int)] = Kleisli(s => Task {
+      println("getUserById ::" + s)
+      //Thread.sleep(1000)
+      ("OldUserName", "OldDepartment", 34)
+    })
+    def getAccountById(key:String):ParMixedTask[(String,String,Double)] = Kleisli(s => Task {
+      println("getAccountById ::" + s)
+      Thread.sleep(2000)
+      ("OldUserName", "OldDepartment", 12.34)
+    })
+  }
+
+
+  implicit val validationInterp = new ValidationOp.Handler[ParMixedTask]{
+    def validateUser(inp:(String,String,Int)):ParMixedTask[Boolean] = Kleisli(s => Task {
+      println("validateUser ::" + s)
+      //Thread.sleep(2000)
+      true
+    })
+    def validateAccount(inp:(String,String,Double)):ParMixedTask[Boolean] = Kleisli(s => Task {
+      println("validateAccount ::" + s)
+      Thread.sleep(4000)
+      true
+    })
+  }
+}
---
+++ src/main/scala/freestyle/effects/FreeStyleEffects.scala
@@ -0,0 +1,87 @@
+package freestyle.effects
+
+/**
+  * Created by abdhesh on 10/04/17.
+  */
+object FreeStyleEffects extends App {
+
+  import freestyle._
+  import freestyle.implicits._
+  import freestyle.effects.error._
+  import freestyle.effects.error.implicits._
+  import cats.implicits._
+
+  val boom = new RuntimeException("BOOM")
+
+  type Target[A] = Either[Throwable, A]
+
+  def shortCircuit[F[_] : ErrorM] =
+    for {
+      a <- FreeS.pure(1)
+      b <- ErrorM[F].either[Int](Left(boom))
+      c <- FreeS.pure(1)
+    } yield a + b + c
+
+  def shortCircuitWithError[F[_] : ErrorM] =
+    for {
+      a <- FreeS.pure(1)
+      b <- ErrorM[F].error[Int](boom)
+      c <- FreeS.pure(1)
+    } yield a + b + c
+
+  def continueWithRightValue[F[_] : ErrorM] =
+    for {
+      a <- FreeS.pure(1)
+      b <- ErrorM[F].either[Int](Right(1))
+      c <- FreeS.pure(1)
+    } yield a + b + c
+
+  import cats.Eval
+
+  def catchingExceptions[F[_] : ErrorM] =
+    for {
+      a <- FreeS.pure(1)
+      b <- ErrorM[F].catchNonFatal[Int](Eval.later(throw new RuntimeException))
+      c <- FreeS.pure(1)
+    } yield a + b + c
+
+  import freestyle.effects.option._
+  import freestyle.effects.option.implicits._
+
+  def programNone[F[_] : OptionM] =
+    for {
+      a <- FreeS.pure(1)
+      b <- OptionM[F].option[Int](None)
+      c <- FreeS.pure(1)
+    } yield a + b + c
+
+  def programSome[F[_] : OptionM] =
+    for {
+      a <- FreeS.pure(1)
+      b <- OptionM[F].option(Some(1))
+      c <- FreeS.pure(1)
+    } yield a + b + c
+
+  def programNone2[F[_] : OptionM] =
+    for {
+      a <- FreeS.pure(1)
+      b <- OptionM[F].none[Int]
+      c <- FreeS.pure(1)
+    } yield a + b + c
+
+  val resultLeft = shortCircuit[ErrorM.Op].exec[Target]
+  val resultRight = continueWithRightValue[ErrorM.Op].exec[Target]
+  println(resultLeft)
+  println(resultRight)
+
+  val catchException = catchingExceptions[ErrorM.Op].exec[Target]
+  println(catchException)
+  val optResult = programNone[OptionM.Op].exec[Option]
+  println(optResult)
+
+  val optResultSome = programSome[OptionM.Op].exec[Option]
+  println(optResultSome)
+
+  val nonResult = programNone2[OptionM.Op].exec[Option]
+  println(nonResult)
+}
